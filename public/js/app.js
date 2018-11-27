6dee8a551447c44c551ae44c1e120e366d113487
nxde <nxde@ctd.com>
2018-11-27 21:27:11 +0700

    Add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,6 @@
 var socket=io();
+var name=getQueryVariable("name") || "Anonymous";
+var room=getQueryVariable("room");
 
 socket.on("connect",function(){
     console.log("Got through  to server with IO");
@@ -7,7 +9,9 @@ socket.on("connect",function(){
 socket.on("message",function(message){
     //console.log(message.text);
     var momentTimestamp=moment.utc(message.timestampt);
-    jQuery(".messages").append("<p><strong>"+ momentTimestamp.local().format("h:mm a")+"</strong>  "+message.text+"</p>");
+    var $messages=jQuery(".messages");
+    $messages.append("<p><strong>"+message.name+" "+ momentTimestamp.local().format("h:mm a")+"</strong></p>");
+    $messages.append("<p>"+message.text+"</p>");
 });
 
 
@@ -17,6 +21,7 @@ $form.on("submit",function(event){
     event.preventDefault();
     var $message=$form.find("input[name=message]");
     socket.emit("message",{
+        name:name,
         text: $message.val()
     });
     $message.val("");
