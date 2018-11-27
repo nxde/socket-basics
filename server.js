dab49a7e4a73e87a5f125f2332087c9135aa4e0d
nxde <nxde@ctd.com>
2018-11-27 16:15:43 +0700

    Fix version for socketIO

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ var io=require("socket.io")(http);
 io.on("connection",function(socket){
     console.log("User connected with IO");
 
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+      });
+
     socket.on("message",function(message){
         console.log("Message received: "+message.text);
-        
+
         //Send to all: socket.broadcast.emit("message",message);
         socket.broadcast.emit("message",message);
     });
@@ -20,6 +24,13 @@ io.on("connection",function(socket){
 });
 
 app.use(express.static(__dirname+"/public"));
+// app.get('/', function(req, res){
+//     res.sendFile(__dirname + '/public/index.html');
+//   });
+
+app.get('/iochat', function(req, res){
+    res.sendFile(__dirname + '/public/iochat.html');
+});
 
 http.listen(PORT, function(){
     console.log("Server  listening");
